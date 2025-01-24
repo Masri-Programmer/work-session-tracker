f64804cf828c7675ed26b959d9c666042713db6d
Masri-Programmer <masri_mohamad@protonmail.com>
2025-01-24 17:22:58 +0100

    some responsive edits

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,33 @@
   <div
     class="font-mono bg-gradient-to-r from-indigo-600 to-purple-600 dark:from-slate-800 dark:to-stone-800 text-white min-h-screen flex items-center justify-center"
   >
-    <div class="w-full max-w-4xl p-8 rounded-xl shadow-2xl">
-      <div class="max-h-[90vh] overflow-y-auto p-8 space-y-8">
-        <h1 class="text-4xl font-extrabold text-center text-white">Work Hours Calculator</h1>
-        <div id="sessions" class="space-y-8">
+    <div id="theme-toggler" class="theme-toggle" title="Toggle theme">
+      <span class="theme-toggle-sr">Toggle theme</span>
+      <svg
+        @click="toggleTheme"
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+        class="theme-toggle__expand bg-slate-300 dark:bg-slate-700 hover:bg-slate-400 dark:hover:bg-slate-600 p-3 rounded-full transition-transform transform hover:scale-105 absolute top-0 right-0 size-10 m-3 lg:w-16 lg:h-16 md:w-12 md:h-12 sm:w-10 sm:h-10 sm:m-4 sm:p-4"
+        fill="currentColor"
+        viewBox="0 0 32 32"
+      >
+        <clipPath id="theme-toggle__expand__cutout">
+          <path d="M0-11h25a1 1 0 0017 13v30H0Z" />
+        </clipPath>
+        <g clip-path="url(#theme-toggle__expand__cutout)">
+          <circle cx="16" cy="16" r="8.4" />
+          <path
+            d="M18.3 3.2c0 1.3-1 2.3-2.3 2.3s-2.3-1-2.3-2.3S14.7.9 16 .9s2.3 1 2.3 2.3zm-4.6 25.6c0-1.3 1-2.3 2.3-2.3s2.3 1 2.3 2.3-1 2.3-2.3 2.3-2.3-1-2.3-2.3zm15.1-10.5c-1.3 0-2.3-1-2.3-2.3s1-2.3 2.3-2.3 2.3 1 2.3 2.3-1 2.3-2.3 2.3zM3.2 13.7c1.3 0 2.3 1 2.3 2.3s-1 2.3-2.3 2.3S.9 17.3.9 16s1-2.3 2.3-2.3zm5.8-7C9 7.9 7.9 9 6.7 9S4.4 8 4.4 6.7s1-2.3 2.3-2.3S9 5.4 9 6.7zm16.3 21c-1.3 0-2.3-1-2.3-2.3s1-2.3 2.3-2.3 2.3 1 2.3 2.3-1 2.3-2.3 2.3zm2.4-21c0 1.3-1 2.3-2.3 2.3S23 7.9 23 6.7s1-2.3 2.3-2.3 2.4 1 2.4 2.3zM6.7 23C8 23 9 24 9 25.3s-1 2.3-2.3 2.3-2.3-1-2.3-2.3 1-2.3 2.3-2.3z"
+          />
+        </g>
+      </svg>
+    </div>
+    <div class="w-full sm:max-w-4xl p-1 sm:p-8 rounded-xl shadow-2xl">
+      <div class="sm:max-h-[90vh] sm:overflow-y-auto p-4 sm:p-8 space-y-8">
+        <h1 class="text-xl sm:text-3xl font-extrabold text-center text-white">
+          Work Hours Calculator
+        </h1>
+        <div id="sessions" class="space-y-4 sm:space-y-8">
           <div
             v-for="(session, index) in sessions"
             :key="index"
@@ -18,115 +41,98 @@
             >
               <LucideX class="w-4 h-4" />
             </button>
-            <div class="time-wrapper flex items-center space-x-4">
+            <div
+              class="time-wrapper flex sm:items-center space-x-4 w-full flex-col sm:flex-row gap-1"
+            >
               <label
                 :for="`session${index + 1}-start`"
-                class="font-semibold text-lg text-slate-800 dark:text-slate-300"
+                class="font-semibold text-base sm:text-lg text-slate-800 dark:text-slate-300"
                 >Session {{ index + 1 }} - Kommen:</label
               >
-              <LucideClock class="text-slate-600 dark:text-slate-400" />
-              <input
-                type="time"
-                :id="`session${index + 1}-start`"
-                v-model="session.start"
-                @input="calculateWorkHours"
-                @paste="handlePaste($event, index, 'start')"
-                class="w-full p-4 rounded-xl border-2 bg-white dark:bg-slate-800 text-slate-900 dark:text-white border-slate-300 dark:border-slate-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 dark:focus:ring-indigo-400 shadow-md transition-transform transform hover:scale-105"
-              />
+              <div class="flex items-center space-x-4 w-full">
+                <LucideClock class="text-slate-600 dark:text-slate-400" />
+                <input
+                  type="time"
+                  :id="`session${index + 1}-start`"
+                  v-model="session.start"
+                  @input="calculateWorkHours"
+                  @paste="handlePaste($event, index, 'start')"
+                  class="w-full p-4 rounded-xl border-2 bg-white dark:bg-slate-800 text-slate-900 dark:text-white text-sm sm:text-base border-slate-300 dark:border-slate-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 dark:focus:ring-indigo-400 shadow-md transition-transform transform hover:scale-105"
+                />
+              </div>
             </div>
-            <div class="time-wrapper flex items-center space-x-4 mt-6">
+            <div
+              class="time-wrapper flex sm:items-center space-x-4 w-full flex-col sm:flex-row gap-1 mt-6"
+            >
               <label
                 :for="`session${index + 1}-end`"
-                class="font-semibold text-lg text-slate-800 dark:text-slate-300"
+                class="font-semibold text-base sm:text-lg text-slate-800 dark:text-slate-300"
                 >Session {{ index + 1 }} - Gehen:</label
               >
-              <LucideClock class="text-slate-600 dark:text-slate-400" />
-              <input
-                type="time"
-                :id="`session${index + 1}-end`"
-                v-model="session.end"
-                @input="calculateWorkHours"
-                @paste="handlePaste($event, index, 'end')"
-                class="w-full p-4 rounded-xl border-2 bg-white dark:bg-slate-800 text-slate-900 dark:text-white border-slate-300 dark:border-slate-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 dark:focus:ring-indigo-400 shadow-md transition-transform transform hover:scale-105"
-              />
+              <div class="flex items-center space-x-4 w-full">
+                <LucideClock class="text-slate-600 dark:text-slate-400" />
+                <input
+                  type="time"
+                  :id="`session${index + 1}-end`"
+                  v-model="session.end"
+                  @input="calculateWorkHours"
+                  @paste="handlePaste($event, index, 'end')"
+                  class="w-full p-4 rounded-xl border-2 bg-white dark:bg-slate-800 text-slate-900 dark:text-white text-sm sm:text-base border-slate-300 dark:border-slate-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 dark:focus:ring-indigo-400 shadow-md transition-transform transform hover:scale-105"
+                />
+              </div>
             </div>
           </div>
         </div>
-        <div class="buttons-container flex flex-wrap justify-center gap-6">
+        <div class="buttons-container flex flex-wrap justify-center gap-3 sm:gap-6">
           <button
             @click="addSession"
-            class="dark:bg-transparent bg-indigo-600 hover:bg-indigo-700 text-white px-8 py-4 rounded-full shadow-lg flex items-center space-x-3 transition-transform transform hover:scale-105"
+            class="dark:bg-transparent text-xs sm:text-base bg-indigo-600 hover:bg-indigo-700 text-white px-4 sm:px-8 py-2 sm:py-4 rounded-full shadow-lg flex items-center space-x-2 sm:space-x-3 transition-transform transform hover:scale-105"
           >
             <LucidePlus class="text-white" />
             <span>Add Session</span>
           </button>
           <button
             @click="addBreak"
-            class="dark:bg-transparent bg-green-600 hover:bg-green-700 text-white px-8 py-4 rounded-full shadow-lg flex items-center space-x-3 transition-transform transform hover:scale-105"
+            class="dark:bg-transparent text-xs sm:text-base bg-green-600 hover:bg-green-700 text-white px-4 sm:px-8 py-2 sm:py-4 rounded-full shadow-lg flex items-center space-x-2 sm:space-x-3 transition-transform transform hover:scale-105"
           >
             <LucideCalculator class="text-white" />
             <span>Add Break</span>
           </button>
           <button
             @click="exportSchedule"
-            class="dark:bg-transparent bg-yellow-600 hover:bg-yellow-700 text-white px-8 py-4 rounded-full shadow-lg flex items-center space-x-3 transition-transform transform hover:scale-105"
+            class="dark:bg-transparent text-xs sm:text-base bg-yellow-600 hover:bg-yellow-700 text-white px-4 sm:px-8 py-2 sm:py-4 rounded-full shadow-lg flex items-center space-x-2 sm:space-x-3 transition-transform transform hover:scale-105"
           >
             <LucideDownload class="text-white" />
             <span>Export Schedule</span>
           </button>
-          <div
-            id="theme-toggler"
-            class="theme-toggle"
-            title="Toggle theme"
-          >
-            <span class="theme-toggle-sr">Toggle theme</span>
-            <svg
-              @click="toggleTheme"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-              width="1em"
-              height="1em"
-              fill="currentColor"
-              class="theme-toggle__expand bg-slate-300 dark:bg-slate-700 hover:bg-slate-400 dark:hover:bg-slate-600 p-4 rounded-full  transition-transform transform hover:scale-105 absolute top-0 right-0 size-16 m-6"
-              viewBox="0 0 32 32"
-            >
-              <clipPath id="theme-toggle__expand__cutout">
-                <path d="M0-11h25a1 1 0 0017 13v30H0Z" />
-              </clipPath>
-              <g clip-path="url(#theme-toggle__expand__cutout)">
-                <circle cx="16" cy="16" r="8.4" />
-                <path
-                  d="M18.3 3.2c0 1.3-1 2.3-2.3 2.3s-2.3-1-2.3-2.3S14.7.9 16 .9s2.3 1 2.3 2.3zm-4.6 25.6c0-1.3 1-2.3 2.3-2.3s2.3 1 2.3 2.3-1 2.3-2.3 2.3-2.3-1-2.3-2.3zm15.1-10.5c-1.3 0-2.3-1-2.3-2.3s1-2.3 2.3-2.3 2.3 1 2.3 2.3-1 2.3-2.3 2.3zM3.2 13.7c1.3 0 2.3 1 2.3 2.3s-1 2.3-2.3 2.3S.9 17.3.9 16s1-2.3 2.3-2.3zm5.8-7C9 7.9 7.9 9 6.7 9S4.4 8 4.4 6.7s1-2.3 2.3-2.3S9 5.4 9 6.7zm16.3 21c-1.3 0-2.3-1-2.3-2.3s1-2.3 2.3-2.3 2.3 1 2.3 2.3-1 2.3-2.3 2.3zm2.4-21c0 1.3-1 2.3-2.3 2.3S23 7.9 23 6.7s1-2.3 2.3-2.3 2.4 1 2.4 2.3zM6.7 23C8 23 9 24 9 25.3s-1 2.3-2.3 2.3-2.3-1-2.3-2.3 1-2.3 2.3-2.3z"
-                />
-              </g>
-            </svg>
-          </div>
         </div>
         <div
           id="result"
-          class="result-container mt-6 p-6 rounded-lg bg-red-600 dark:bg-slate-600 text-white text-center shadow-md"
+          class="result-container p-3 my-3 sm:mt-6 sm:p-6 rounded-lg bg-red-600 dark:bg-slate-600 text-white text-center shadow-md text-sm sm:text-base"
         >
           {{ resultMessage }}
         </div>
         <div
           id="result"
           @click="copySchedule"
-          class="result-container mt-6 p-6 rounded-lg bg-red-600 dark:bg-slate-600 text-white text-center shadow-md flex gap-1 justify-center cursor-pointer"
+          class="result-container p-3 my-3 sm:mt-6 sm:p-6 rounded-lg bg-red-600 dark:bg-slate-600 text-white text-center shadow-md text-sm sm:text-base flex gap-1 justify-center cursor-pointer"
         >
           <LucideClipboard v-if="!copied" class="text-white" />
           <CheckCheck v-if="copied" class="text-white" />
-          {{ scheduleText }}
+         <p class="max-w-[90%]"> {{ scheduleText }}</p>
         </div>
       </div>
     </div>
   </div>
-  <div class="fixed bottom-0 left-0 m-4 text-white flex flex-col space-y-2">
+  <div class="hidden sm:fixed bottom-0 left-0 m-4 text-white text-sm sm:flex flex-col space-y-2">
     <OnlineTracker />
     <TimeCounter />
     FPS: {{ fps }}
     <div v-if="isSupported">Battery: {{ level * 100 }}%</div>
   </div>
-  <div class="absolute bottom-4 right-4 dark:text-gray-600 font-serif text-lg italic opacity-70">
+  <div
+    class="hidden sm:block absolute bottom-4 right-4 dark:text-gray-600 font-serif text-base sm:text-lg italic opacity-70"
+  >
     <span>Masri</span>
   </div>
 </template>
@@ -231,7 +237,7 @@ const exportSchedule = () => {
 
 const toggleTheme = () => {
   isDarkMode.value = !isDarkMode.value
-  var element = document.getElementById('theme-toggler')
+  const element = document.getElementById('theme-toggler')
   if (isDarkMode.value) {
     document.documentElement.classList.add('dark')
     element.classList.add('theme-toggle--toggled')
@@ -242,10 +248,12 @@ const toggleTheme = () => {
 }
 
 onMounted(() => {
-  const storedTheme = sessionStorage.getItem('darkMode')
-  if (storedTheme === 'true') {
-    isDarkMode.value = true
+  if (isDarkMode.value) {
     document.documentElement.classList.add('dark')
+    const element = document.getElementById('theme-toggler')
+    if (element) {
+      element.classList.add('theme-toggle--toggled')
+    }
   }
 })
 
@@ -289,8 +297,10 @@ const copySchedule = () => {
   display: flex;
   justify-content: space-between;
   margin-top: 1rem;
-  .lucide {
-    margin: 5px 5px;
-  }
+}
+.lucide {
+  margin: 5px 5px;
+  width: 15px;
+  height: 15px;
 }
 </style>
